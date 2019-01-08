47bb8a5f616371297c5f947d154a149a1f079a1d
Andrey Rodionov <rodionoff2007@mail.ru>
2019-01-08 15:52:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$('.carousel').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+});	
+
+
+$('.field-select').chosen({
+	disable_search_threshold: 10,
+});
+
+});
