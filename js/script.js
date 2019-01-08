7fea920bc35e806fd23082daf3c3c12aca120fea
Andrey Rodionov <rodionoff2007@mail.ru>
2019-01-09 02:25:25 +0300

    mobile version almost done

--- js/script.js
+++ js/script.js
@@ -12,3 +12,12 @@ $('.field-select').chosen({
 });
 
 });
+
+
+var toggler = document.getElementById('toggler');
+toggler.addEventListener('click', mainNavVisibleToggle);
+function mainNavVisibleToggle(e) {
+  e.preventDefault();
+  toggler.classList.toggle('toggler--close');
+  document.getElementById('nav').classList.toggle('nav--visible');
+}
