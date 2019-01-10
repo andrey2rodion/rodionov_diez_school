847bd4b0685acd47458a5bb20c597bef34e73b2e
Andrey Rodionov <rodionoff2007@mail.ru>
2019-01-11 00:32:48 +0300

    desctop version

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 $('.carousel').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-
+  arrows: true,
+  prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });	
 
 
